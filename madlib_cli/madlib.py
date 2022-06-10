d3d5ed9a9a89f587f808e42403179679f9d5adaf
unknown <lailaibraheem@hotmail.com>
2022-06-10 16:26:01 +0300

    editeddone

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,67 +1,46 @@
-def read_template(path):
+import re
+
+def read_template(file_name):
     try:
-        with open(path, 'r') as file:
+        with open(file_name, 'r') as file:
             content = file.read()
             return content
-    except FileNotFoundError as err:
-        return err
+    except FileNotFoundError:
+        raise FileNotFoundError
 
-def parse_template(path):
-    with open(path, 'r') as file:
-        content = file.read()
-        x = content.replace("{Noun}", "{}")
-        y = x.replace("{Adjective}", "{}")
-        z = ("Adjective", "Adjective", "Noun")
-        return y,z
-# print(parse_template("../assets/dark_and_stormy_night_template.txt"))
-def merge(string1, values):
-    with open("../assets/solved_game.txt", 'w') as file:
-        file.write(string1.format(*values))
-    return string1.format("dark", "stormy", "night")
+def parse_template(text):
+    reg = '\{(.*?)\}'
+    prompts = re.findall(reg, text)
+    text = re.sub(reg, '{}', text)
+    return text, tuple(prompts)
+
+def get_user_responses(prompts):
+    inputs = []
+    for i in prompts:
+        inputs.append(input(f'Enter a {i.lower()}: '))
+    return inputs
+
+def merge(text, inputs):
+    return text.format(*inputs)
+
+def print_formatted_text(text):
+    print(text)
+
+def write_to_file(filename, text):
+    with open(filename, 'w') as out_file:
+        out_file.write(text)
+
+def run():
+    filename = '../assets/madlib.txt'
+    text = read_template(filename)
+    text, prompts = parse_template(text)
+    inputs = get_user_responses(prompts)
+    text = merge(text, inputs)
+    print_formatted_text(text)
+    out_filename = '../assets/solved_template.txt'
+    write_to_file(out_filename, text)
 
 if __name__ == '__main__':
     print("Welcome to Madlib game you will enter a few words and you will have fun with us.")
-    inputs = []
-    adj = input("Enter an Adjective ")
-    inputs.append(adj)
-    adj2 = input("Enter an Adjective ")
-    inputs.append(adj2)
-    first = input("Enter your First Name ")
-    inputs.append(first)
-    past = input("Enter a Past tense verb ")
-    inputs.append(past)
-    first2 = input("Enter a Name ")
-    inputs.append(first2)
-    adj3 = input("Enter an Adjective ")
-    inputs.append(adj3)
-    adj4 = input("Enter an Adjective ")
-    inputs.append(adj4)
-    plural = input("Enter a Plural noun ")
-    inputs.append(plural)
-    large = input("Enter a Large animal ")
-    inputs.append(large)
-    small = input("Enter a Small animal ")
-    inputs.append(small)
-    girl = input("Enter a Girls Name ")
-    inputs.append(girl)
-    adj5 = input("Enter an Adjective ")
-    inputs.append(adj5)
-    plural2 = input("Enter a Plural noun ")
-    inputs.append(plural2)
-    adj6 = input("Enter an Adjective ")
-    inputs.append(adj6)
-    plural3 = input("Enter a Plural noun ")
-    inputs.append(plural3)
-    numb = input("Enter a Number between 1-50 ")
-    inputs.append(numb)
-    first3 = input("Enter a Name ")
-    inputs.append(first3)
-    numb2 = input("Enter a Number ")
-    inputs.append(numb2)
-    plural4 = input("Enter a Plural noun ")
-    inputs.append(plural4)
-    numb3 = input("Enter a Number ")
-    inputs.append(numb3)
-    plural5 = input("Enter a Plural noun ")
-    inputs.append(plural5)
+    run()
 
